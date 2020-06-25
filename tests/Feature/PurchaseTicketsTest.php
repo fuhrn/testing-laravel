f04ba4a288da5b7dacfd3fbaf03fe5090e7e52b2
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-06-25 08:09:02 -0300

    Outlining the First Purchasing Test

---
+++ tests/Feature/PurchaseTicketsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Concert;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PurchaseTicketsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     * $test
+     * @return void
+     */
+    public function customer_can_purchase_concert_tickets()
+    {
+        $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
+
+
+        $response = $this->postJson('/concerts/{$concert->id}/orders', [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => $paymentGateway->getValidTestToken(),
+        ]);
+
+        $this->assertEquals(9750, $paymentGateway->totalCharges());
+
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNotNull($order);
+        $this->assertEquals(3, $order->tickets->count());
+
+    }
+}
