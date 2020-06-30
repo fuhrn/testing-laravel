47ca82f7bcf09e32a66770716f18850f42b35a41
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-06-30 10:13:54 -0300

    Uh Oh, a Segfault

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -57,9 +57,7 @@ class PurchaseTicketsTest extends TestCase
         ]);
 
         $this->assertEquals(9750, $this->paymentGateway->totalCharges());
-
         $this->assertTrue($concert->hasOrderFor('john@example.com'));
-
         $this->assertEquals(3, $concert->ordersFor('john@example.com')->first()->ticketQuantity());
     }
 
@@ -86,17 +84,36 @@ class PurchaseTicketsTest extends TestCase
 
     /**
      * @test
-     * @group
+     * @group 1
      */
     public function cannot_purchase_tickets_another_customer_is_already_trying_to_purchase()
     {
-        $concert = factory(Concert::class)->states('unpublished')->create()->addTickets(3);
+        $concert = factory(Concert::class)->states('unpublished')->create([
+            'ticket_price' => 1200
+        ])->addTickets(3);
+
+        $this->paymentGateway->beforeFirstCharge(function ($paymentGateway) use ($concert) {
+            $response = $this->orderTickets($concert, [
+                'email' => 'personB@example.com',
+                'ticket_quantity' => 3,
+                'payment_token' => $this->paymentGateway->getValidTestToken(),
+            ]);
+
+            $response->assertStatus(422);
+            $this->assertFalse($concert->hasOrderfor('personB@example.com'));
+            $this->assertEquals(0, $this->paymentGateway->totalCharges());
+        });
+
 
         $response = $this->orderTickets($concert, [
             'email' => 'personA@example.com',
             'ticket_quantity' => 3,
             'payment_token' => $this->paymentGateway->getValidTestToken(),
         ]);
+
+        $this->assertEquals(3600, $this->paymentGateway->totalCharges());
+        $this->assertTrue($concert->hasOrderFor('personA@example.com'));
+        $this->assertEquals(3, $concert->ordersFor('personA@example.com')->first()->ticketQuantity());
     }
 
 
