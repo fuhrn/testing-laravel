be5168dba75170ef7ed6222f7d81c5789f097bf1
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-07-01 13:51:05 -0300

    Deleting Stale Tests

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -48,20 +48,4 @@ class OrderTest extends TestCase
         ], $result);
     }
 
-    /**
-     * @test
-     */
-    public function tickets_are_released_when_an_order_is_cancelled()
-    {
-        $concert = factory(Concert::class)->create()->addTickets(10);
-        $order = $concert->orderTickets('jane@example.com', 5);
-        $this->assertEquals(5, $concert->ticketsRemaining());
-
-        $order->cancel();
-
-        $this->assertEquals(10, $concert->ticketsRemaining());
-        $this->assertNull(Order::find($order->id));
-
-
-    }
 }
