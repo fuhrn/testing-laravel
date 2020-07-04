9d9852eefa5ef533b79a55275f8061e8d9d98902
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-07-04 13:37:55 -0300

    Generating a Valid Payment Token

---
+++ tests/Feature/StripePaymentGatewayTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Stripe\Stripe;
+use Tests\TestCase;
+
+class StripePaymentGatewayTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     * @test
+     * @group 1
+     */
+    public function charges_with_a_valid_payment_token_are_successful()
+    {
+        $paymentGateway = new StripePaymentGateway;
+
+        $token = \Stripe\Token::create([
+            'card' => [
+                'number' => '4242424242424242',
+                'exp_month' => 1,
+                'exp_year' => date('Y') + 1,
+                'cvc' => '123',
+            ],
+        ], ['api_key' => config('services.stripe.secret')])->id;
+
+        $paymentGateway->charge(2500, $token);
+
+    }
+}
