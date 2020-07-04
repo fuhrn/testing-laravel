7d4a96cd2ec5916ac0821401e871710095754f10
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-07-04 19:21:20 -0300

    Don't Mock What You Don't Own

--- app/Billing/StripePaymentGateway.php
+++ app/Billing/StripePaymentGateway.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Billing;
+use Illuminate\Support\Facades\Http;
+
 
 use Stripe\Charge;
 
 class StripePaymentGateway implements PaymentGateway
 {
+    private $apiKey;
+
     public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
@@ -19,3 +23,44 @@ class StripePaymentGateway implements PaymentGateway
         ], ['api_key' => $this->apiKey]);
     }
 }
+
+//class StripePaymentGateway implements PaymentGateway
+//{
+//    private $apiKey;
+//
+//    public function __construct($apiKey)
+//    {
+//        $this->apiKey = $apiKey;
+//    }
+//    public function charge($amount, $token)
+//    {
+//        dd([$this->apiKey]);
+//        (new \GuzzleHttp\Client)->post('https:://api.stripe.com/v1  /charges', [
+//            'headers' => [
+//                'Authorization' => "Bearer {$this->apiKey}",
+//            ],
+//            'form_params' => [
+//                'amount' => $amount,
+//                'source' => $token,
+//                'currency' => 'usd',
+//                ]
+//            ]);
+
+//        Http::withHeaders([
+//            'Authorization' => "Bearer {$this->apiKey}",
+//        ])->post('https:://api.stripe.com/v1/charges', [
+//            'amount' => $amount,
+//            'source' => $token,
+//            'currency' => 'usd',
+//        ]);
+//
+//    }
+//}
+
+//$response = Http::withHeaders([
+//    'Authorization' => "Bearer {$this->apiKey}",
+//    ])->post('https:://api.stripe.com/v1/charges', [
+//    'amount' => $amount,
+//    'source' => $token,
+//    'currency' => 'usd',
+//]);
