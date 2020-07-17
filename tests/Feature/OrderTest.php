9b7e1fc2e94363e71bd01bf5337818df6b0e964a
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-07-17 09:11:39 -0300

    Extracting a Finder Method

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -6,6 +6,7 @@ use App\Concert;
 use App\Order;
 use App\Reservation;
 use App\Ticket;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -23,12 +24,45 @@ class OrderTest extends TestCase
         $concert = factory(Concert::class)->create()->addTickets(5);
         $this->assertEquals(5, $concert->ticketsRemaining());
 
-        $order = Order::forTickets($concert->findTickets(3),'john@example.com', 3600);
+        $order = Order::forTickets($concert->findTickets(3), 'john@example.com', 3600);
 
         $this->assertEquals('john@example.com', $order->email);
         $this->assertEquals(3, $order->ticketQuantity());
         $this->assertEquals(3600, $order->amount);
         $this->assertEquals(2, $concert->ticketsRemaining());
+
+    }
+
+
+    /**
+     * @test
+     * @group
+     */
+    public function retrieving_an_order_by_confirmation_number()
+    {
+        $order = factory(Order::class)->create([
+
+            'confirmation_number' => 'ORDERCONFIRMATION1234'
+        ]);
+
+        $foundOrder = Order::findByConfirmationNumber('ORDERCONFIRMATION1234');
+
+        $this->assertEquals($order->id, $foundOrder->id);
+    }
+
+    /**
+     * @test
+     * @group 1
+     */
+    public function retrieving_an_nonexistent_order_by_confirmation_number_throws_an_exception()
+    {
+        try {
+            Order::findByConfirmationNumber('NONEXISTENTCONFIRMATIONNUMBER');
+        } catch (ModelNotFoundException $e) {
+            return;
+        }
+
+        $this->fail('No matchig order was found for the specified confirmation number, but an exception was not thrown');
 
     }
 
