e104d727c723c31c47b77090d893af22acba1da2
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-08-06 18:59:46 -0300

    Refactoring for Robutness

--- app/Jobs/SendAttendeeMessage.php
+++ app/Jobs/SendAttendeeMessage.php
@@ -33,8 +33,10 @@ class SendAttendeeMessage implements ShouldQueue
      */
     public function handle()
     {
-        $this->attendeeMessage->recipients()->each(function ($recipient) {
-            Mail::to($recipient)->send(New AttendeeMessageEmail($this->attendeeMessage));
+        $this->attendeeMessage->withChunkedRecipients(20, function ($recipients) {
+            $recipients->each(function ($recipient) {
+            Mail::to($recipient)->queue(New AttendeeMessageEmail($this->attendeeMessage));
+            });
         });
     }
 }
